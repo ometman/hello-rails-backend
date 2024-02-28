4217398c8587605913d4df7b4e7b4d3f2751e2b3
ometman <revomet@gmail.com>
2024-02-28 10:45:33 +0000

    Add namespace api route and refactor controller directory and implement forgery protection

--- app/controllers/api/greetings_controller.rb
+++ app/controllers/api/greetings_controller.rb
@@ -1,4 +1,6 @@
 class GreetingsController < ApplicationController
+    skip_before_action :verify_authenticity_token
+
     def random_message
         render json: Greeting.order("RANDOM()").first
     end

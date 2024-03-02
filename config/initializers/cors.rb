b70b782459dc3f7d17677728061a612f007783c3
ometman <revomet@gmail.com>
2024-03-02 16:31:44 +0000

    Add and configure cors

---
+++ config/initializers/cors.rb
@@ -0,0 +1,10 @@
+# config/initializers/cors.rb
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins 'http://localhost:3001'
+    resource '/api/greetings/*',
+      headers: :any,               # Allow any headers in requests
+      methods: [:get, :post, :put, :patch, :delete, :options],  # Allow these HTTP methods
+      credentials: true           # Allow credentials (e.g., cookies, HTTP authentication)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   namespace :api do
     resource :greetings, to: 'greetings#random'
   end
+
   # Defines the root path route ("/")
   root "api/greetings#random"
 end
